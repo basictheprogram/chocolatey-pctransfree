c3f9b1b5d30a072d95ac65d92bf60a1c3ae888c3
Bob Tanner <tanner@real-time.com>
2017-11-28 14:26:17 -0600

    Initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageArgs = @{
+  packageName    = 'pctransfree'
+  fileType       = 'EXE'
+  url            = 'http://download.easeus.com/free/pctrans.exe'
+  checksum       = '3913cbc17881700c4b35a31cb5f33f0eaa2a2a1b6ca7c471a8ce8351158367a9'
+  checksumType   = 'sha256'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+  softwareName   = 'EaseUS Todo PCTrans (Free)'
+}
+
+Install-ChocolateyPackage @packageArgs
