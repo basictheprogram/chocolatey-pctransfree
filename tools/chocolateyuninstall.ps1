e12502b46893896ffa58fb1f5e42c727569ab427
Bob Tanner <tanner@real-time.com>
2018-05-17 18:39:55 +0100

    Added uninstall script

---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,32 @@
+$ErrorActionPreference = 'Stop';
+
+# HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall
+# HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\
+#
+$msiId       = '{4AB6A079-178B-4144-B21F-4D1AE71666A2}'
+$msiId64     = '{2180B33F-3225-423E-BBC1-7798CFD3CD1F}'
+$packageName = 'pctransfree'
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName 'EaseUS Todo PCTrans*'
+
+if ($key.Count -eq 1) {
+  $key | ForEach-Object {
+  $packageArgs = @{
+    packageName    = $packageName
+    fileType       = 'EXE'
+    silentArgs     = '/SILENT'
+    validExitCodes = @(0)
+    file           = "$($_.UninstallString)"
+  }
+
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
+}
